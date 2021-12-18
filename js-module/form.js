d438b471b9067df0c16ada5d2ad8c9efa90c91ca
sathvik <sathvikparitala@gmail.com>
2021-12-18 12:45:40 +0530

    code update

--- js-module/form.js
+++ js-module/form.js
@@ -1,5 +1,24 @@
+
+// Syntax
+// function <nameOFFunction>(<params>){}
+
 function handleSubmit(){
     console.log("Submitting");
     alert("Submitting");
-    
+
 }
+// syntax to create varibales
+var num = 10; // number
+var name="syz";
+num = "Sathvik"; // string;
+console.log(num);
+
+
+
+// DataTypes in JS
+String
+Number
+Date
+undefined
+Object
+Boolean
