90ce271a414c7aa51600da444b09cb6217fbf891
sathvik <sathvikparitala@gmail.com>
2021-12-25 18:38:39 +0530

    js

---
+++ js-module/wattsapp/string.js
@@ -0,0 +1,13 @@
+//This is not an annay//
+var cars="Tesla, Audi, BMW, Suzuki";
+//substring//
+const result = cars.substring(7,20);
+console.log(result);
+//Length of string
+console.log(cars.length);
+//replace string
+const modifiedString =cars.replace("Audi","Tata");
+console.log(modifiedString);
+//upper and lower case
+console.log(toUpperCase());
+console.log(toLowerCase());
---
+++ js-module/wattsapp/template-string.js
@@ -0,0 +1,15 @@
+var obj = {
+    name: "sathvik Paritala",
+    age: 23,
+    location: "Unknown",
+    phone: 886445225455,
+    dept: "Finance"
+};
+
+// Name: , age, location: , phone: 
+
+console.log("Name: "+obj.name+",  age: "+obj.age);
+
+// Template string
+
+console.log(`Name = ${obj.name}, Age = ${obj.age}, Dept = ${obj.dept}, Location = ${obj.location}, phone=${obj.phone}`)
